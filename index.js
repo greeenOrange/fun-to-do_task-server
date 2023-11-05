b34a37d5b6bf1dcc3a5b4233ef2ec731423ad070
greeenOrange <gm1sadiq@gmail.com>
2023-11-06 01:01:29 +0600

    server

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://fun-todo_task:1OOUYRe96OBwkEF4@cluster0.jbgbo.mongodb.net/?retryWrites=true&w=majority`
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+app.get('/', (req, res) => {
+    res.send('welcome fun-to-do_task!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`To Do Task Manager ${port}`)
+  })
